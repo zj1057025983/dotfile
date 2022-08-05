f1aece4cdd2d47e6939330177b15e81396f05767
Wang Xiaolin <wx672ster@gmail.com>
2022-08-05 08:56:44 +0800

    trying doom emacs mv tmux.conf to $XDG_CONFIG_HOME/tmux/ add run-or-raise add qt5ct but not used. UI fonts too small. gtk3 works fine. qute-pass works add a Gimp large-font theme make xpdf() a standalone script lpdf rewrote .local/bin/rfc

--- dot.local/bin/tmux-agents.sh
+++ dot.local/bin/tmux-agents.sh
@@ -3,13 +3,12 @@
 set -euC
 
 CONF="~/.config/clash"
-cd
 
 tmux set -w remain-on-exit on
 
-tmux split-window -h
-tmux split-window -vfl99
+tmux split-window -hl75%
+tmux split-window -vfl3
 
 tmux send-keys -t{top-left}  "clash -f $CONF/fy-55549.yaml" C-m
-tmux send-keys -t{top-right} "cd $CONF/yml" C-m "clash" C-m
-tmux send-keys -t{bottom}    "cd $CONF/yml" C-m C-l
+tmux send-keys -t{top-right} "clash.sh" C-m
+tmux send-keys -t{bottom}    "cd $CONF" C-m C-l
